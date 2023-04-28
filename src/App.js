773ca5547415dbd4359744e40bb8c6cfc59a90f7
K Balaji Naik <101827986+Balaji91221@users.noreply.github.com>
2023-04-29 01:35:09 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Auth from './pages/Auth';
 import Home from './pages/Home';
 import PersonalInfo from './pages/PersonalInfo';
+import CircularProgresss from './components/CircularProgresss';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path= "/" element={<Auth />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/PersonalInfo" element={<PersonalInfo />} />
+        <Route path="/circular" element={<CircularProgresss />} />
       </Routes>
     </Router>
   );
